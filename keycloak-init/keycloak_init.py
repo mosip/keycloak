10b38f12ecfcc3a4f2e3f0a421c49c654d949962
Mohan E <mohanraj1715@gmail.com>
2024-08-05 11:57:05 +0530

    Updated keycloak_init.py
    Signed-off-by: Mohan E <mohanraj1715@gmail.com>

--- keycloak-init/keycloak_init.py
+++ keycloak-init/keycloak_init.py
@@ -505,7 +505,7 @@ class KeycloakSession:
             
             # Prepare URL and payload for raw_post
             URL = 'admin/realms/{realm-name}/clients/{client_id}/roles'
-            params_path = {"realm-name": self.keycloak_admin.realm_name, "client-id": client_id}
+            params_path = {"realm-name": self.keycloak_admin.realm_name, "client_id": client_id}
             payload = json.dumps(role_representation)
             
             # Perform the POST requests
