98c18e0c8a943d0f2b47e5999556b7f9cb52f4cb
bhumi46 <thisisbn46@gmail.com>
2025-09-12 18:09:05 +0530

    [MOSIP-42914]updataed chart version
    Signed-off-by: bhumi46 <thisisbn46@gmail.com>

--- deploy/install.sh
+++ deploy/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ]; then
 fi
 NS=keycloak
 SERVICE_NAME=keycloak
-ISTIO_ADDONS_CHART_VERSION=0.0.1-develop
+ISTIO_ADDONS_CHART_VERSION=12.0.3-develop
 
 echo Creating $NS namespace
 kubectl create ns $NS
