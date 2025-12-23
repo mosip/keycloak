166c9576256ca8c12997ced45e5b8aa99c2069f2
Prafulrakhade <prafulrakhade02@gmail.com>
2025-12-23 11:47:42 +0530

    [DSD-9524] Platform 1.2.1.0 GA release
    Signed-off-by: Prafulrakhade <prafulrakhade02@gmail.com>

--- deploy/install.sh
+++ deploy/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ]; then
 fi
 NS=keycloak
 SERVICE_NAME=keycloak
-ISTIO_ADDONS_CHART_VERSION=0.0.1-develop
+ISTIO_ADDONS_CHART_VERSION=1.0.0
 
 echo Creating $NS namespace
 kubectl create ns $NS
@@ -125,8 +125,8 @@ function installing_keycloak() {
 
   echo Installing
   helm -n $NS install $SERVICE_NAME mosip/keycloak --version "7.1.18" \
-    --set image.repository=mosipqa/mosip-artemis-keycloak \
-    --set image.tag=1.3.x \
+    --set image.repository=mosipid/mosip-artemis-keycloak \
+    --set image.tag=1.3.0\
     --set image.pullPolicy=Always \
     --set postgresql.image.repository="mosipid/postgresql" \
     --set postgresql.image.tag="14.2.0-debian-10-r70" \
